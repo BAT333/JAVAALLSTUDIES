2e23adb02365a6a56234f3eedd67d6e4d2805f4b
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-03-04 11:38:53 -0300

    NoteJPA

--- JAVAALLSTUDIES/src/main/java/org/example/Revisao/Main.java
+++ JAVAALLSTUDIES/src/main/java/org/example/Revisao/Main.java
@@ -26,6 +26,7 @@ public class Main {
             System.out.println(mostra);
 
         }
+
         serviceEnderecoEntidade.fechar();
         /*
         ServiceEndereco serviceEndereco = new ServiceEndereco(endereco);
